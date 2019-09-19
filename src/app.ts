8fac59d9abd1eed9826c9b7ab83940a74a8e4b9c
sumeragizzz <developer@sumeragizzz.dev>
2019-09-19 22:47:40 +0900

    メッセージを解析しポストバックデータに変換するよう改修

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { TemplateMessage, TextMessage, WebhookEvent, WebhookRequestBody, Message } from '@line/bot-sdk'
+import { WebhookEvent, WebhookRequestBody, Message, TextMessage, TemplateMessage } from '@line/bot-sdk'
 
 function doPost(e: GoogleAppsScript.Events.DoPost) {
   // 署名検証
@@ -8,15 +8,17 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
   const requestBody: WebhookRequestBody = JSON.parse(e.postData.contents)
   const event: WebhookEvent = requestBody.events[0]
   if (event.type === 'message') {
-    if (event.message.type === "text") {
-      replyMessage(accessToken, event.replyToken, createConfirmMessage(`「${event.message.text}」を受信しました。`))
+    if (event.message.type === 'text') {
+      const enabledBillsData: EnabledBillsData = {enabled : true, data :BillsData.parse(event.message.text)}
+      const disabledBillsData: DisabledBillsData = {enabled : false}
+      replyMessage(accessToken, event.replyToken, createConfirmMessage(`「${JSON.stringify(enabledBillsData.data)}」を受信しました。`, enabledBillsData, disabledBillsData))
     }
   }
   if (event.type === 'postback') {
     replyMessage(accessToken, event.replyToken, createTextMessage(event.postback.data))
   }
 
-  return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON)
+  return ContentService.createTextOutput(JSON.stringify({ 'content': 'post ok' })).setMimeType(ContentService.MimeType.JSON)
 }
 
 function replyMessage(accessToken: string, replyToken: string, message: Message): GoogleAppsScript.URL_Fetch.HTTPResponse {
@@ -37,32 +39,49 @@ function replyMessage(accessToken: string, replyToken: string, message: Message)
 
 function createTextMessage(text: string): TextMessage {
   return {
-    type: "text",
+    type: 'text',
     text: text
   }
 }
 
-function createConfirmMessage(text: string): TemplateMessage {
+function createConfirmMessage(text: string, yesData: EnabledBillsData, noData: DisabledBillsData): TemplateMessage {
   return {
-    type: "template",
-    altText: "cannot display template message",
+    type: 'template',
+    altText: 'cannot display template message',
     template: {
-      type: "confirm",
+      type: 'confirm',
       text: text,
       actions: [
         {
-          type: "postback",
-          label: "はい",
-          displayText: "はい",
-          data: "true"
+          type: 'postback',
+          label: 'はい',
+          displayText: 'はい',
+          data: JSON.stringify(yesData)
         },
         {
-          type: "postback",
-          label: "いいえ",
-          displayText: "いいえ",
-          data: "false"
+          type: 'postback',
+          label: 'いいえ',
+          displayText: 'いいえ',
+          data: JSON.stringify(noData)
         }
       ]
     }
   }
 }
+
+class BillsData {
+  constructor(public dateString: string, public hospital: string, public amount: number) {
+  }
+
+  static parse(text: string): BillsData {
+    const lines = text.split(/\r\n|\n|\r/)
+    const dateString: string = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'yyyy/MM/dd')
+    const hospital: string = lines[0]
+    const amount: number = parseInt(lines[1])
+    return new BillsData(dateString, hospital, amount)
+  }
+}
+
+type EnabledBillsData = {enabled: true, data : BillsData}
+type DisabledBillsData = {enabled: false}
+type BillsPostBackData = EnabledBillsData | DisabledBillsData
